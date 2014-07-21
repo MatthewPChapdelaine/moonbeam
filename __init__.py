5b87ab76a0da968655482e608b01ffd8107fb9c2
Apelsin <appelsindrikken@gmail.com>
2014-07-20 19:43:30 -0700

    dev main

---
+++ __init__.py
@@ -0,0 +1,12 @@
+"""
+Moonbeam (development main file)
+@summary: See README.md for information.
+@author: Vincent "Apelsin" Brubaker-Gianakos
+@email: appelsindrikken@gmail.com
+@date: July 2014
+"""
+
+from moonbeam import app
+
+if __name__ == '__main__':
+    app.run()
