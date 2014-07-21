9c6ed08f0e853a7f71836c635f839baaa0b9700f
Apelsin <appelsindrikken@gmail.com>
2014-07-21 15:25:26 -0700

    Need to test production env

--- data.py
+++ data.py
@@ -15,8 +15,8 @@ class RedisApp():
         port = result.group(4)
         name = result.group(5)
         settings = {'host':host, 'password':password, 'port':port, 'db':name}
-        if DEBUG:
-            print 'Connecting to Redis server (%s)' % ', '.join(['%s=%s' % (k,v) for k, v in settings.iteritems()])
+        #if DEBUG:
+        print 'Connecting to Redis server (%s)' % ', '.join(['%s=%s' % (k,v) for k, v in settings.iteritems()])
         return redis.StrictRedis(**settings)
 
 if 'REDIS_APP_URL_STUNNEL' in os.environ:
